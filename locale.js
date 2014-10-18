dc469d49d7cdc699ca04b7bf6b0e8cd03555162b
giszhangpeng <giszhangpeng@gmail.com>
2014-10-18 22:38:04 +0800

    Create locale.js

---
+++ locale.js
@@ -0,0 +1,10 @@
+
+hcms_acc_monthaccdecanalyse:$.i18n.prop('hcms_acc_monthaccdecanalyse')
+﻿hcms_acc_timeformatwrong:$.i18n.prop('﻿hcms_acc_timeformatwrong')
+hcms_acc_totaltimes:$.i18n.prop('hcms_acc_totaltimes')
+hcms_acc_analyse:$.i18n.prop('hcms_acc_analyse')
+hcms_acc_share:$.i18n.prop('hcms_acc_share')
+hcms_acc_nulldataintime:$.i18n.prop('hcms_acc_nulldataintime')
+hcms_acc_monthstatistic:$.i18n.prop('hcms_acc_monthstatistic')
+hcms_acc_accdecanalyse:$.i18n.prop('hcms_acc_accdecanalyse')
+
